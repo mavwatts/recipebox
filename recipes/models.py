20ce904cf388455fcb50712ea0e24be535315ef6
Amanda Yonce <amanda.stork@gmail.com>
2020-09-14 16:59:29 -0400

    done

--- recipes/models.py
+++ recipes/models.py
@@ -15,20 +15,25 @@ Instructions (TextField)
 """
 
 class Author(models.Model):
-    name = models.CharField(max_length=80)
-    bio = models.TextField(max_length=80)
+    name = models.CharField(max_length=80, null=True)
+    bio = models.TextField(max_length=80, default="")
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=80)
+    title = models.CharField(max_length=80, null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
-    time_required = models.CharField(max_length=50)
+    time_required = models.CharField(max_length=50, null=True)
     instructions = models.TextField()
     
 
     def __str__(self):
         return f"{self.title} - {self.author.name}"
+
+
+class Favorites(models.Model):
+    person = models.ForeignKey(Author, on_delete=models.CASCADE )
+    food = models.ForeignKey(Recipe, on_delete=models.CASCADE )
